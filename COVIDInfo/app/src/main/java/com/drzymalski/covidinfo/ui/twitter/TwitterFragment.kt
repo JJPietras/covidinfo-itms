9e7f9c3bfa1fc9c674daa44fe0473d3810f15956
Jarosław Drzymalski <jarek.drzymalski@gmail.com>
2020-11-27 14:34:46 +0100

    95% Twittera działa
    Wszystko działa, trzeba tylko popoprawiać niektóre rzeczy.

--- COVIDInfo/app/src/main/java/com/drzymalski/covidinfo/ui/twitter/TwitterFragment.kt
+++ COVIDInfo/app/src/main/java/com/drzymalski/covidinfo/ui/twitter/TwitterFragment.kt
@@ -1,12 +1,18 @@
 package com.drzymalski.covidinfo.ui.twitter
 
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.os.StrictMode
 import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.LinearLayout.LayoutParams
@@ -51,7 +57,7 @@ class TwitterFragment : Fragment() {
     fun getPosts(): Unit {
         val twitterScraper = TwitterScraper.builder().build()
         val tweets = twitterScraper.getUserTimeline("MZ_GOV_PL")
-        for ((index,tweet) in tweets.withIndex()) {
+        for (tweet in tweets) {
             println(tweet)
             val card_view = CardView(requireContext())
 
@@ -62,7 +68,6 @@ class TwitterFragment : Fragment() {
             )
 
             cv_layoutParams.setMargins(0,40,0,0)
-            card_view.id = index
             card_view.layoutParams = cv_layoutParams
             card_view.setContentPadding(25,25,25,25)
 
@@ -117,6 +122,42 @@ class TwitterFragment : Fragment() {
             textView12.setTextColor(Color.parseColor("#7EFFFFFF"))
             textView12.text = "Ministerstwo Zdrowia"
             linearLayout2.addView(textView12)
+
+            //2 textView w drugim LL
+            val textView22 = TextView(requireContext())
+            val tv22_layoutParams = LayoutParams(
+                0, // CardView width
+                LayoutParams.WRAP_CONTENT,
+                0.1F // CardView height
+            )
+//            textView12.setPadding(25,10,25,10)
+            tv22_layoutParams.setMargins(50,20,0,0)
+            textView22.layoutParams = tv22_layoutParams
+            val typeface2 = ResourcesCompat.getFont(requireContext(), R.font.roboto)
+            textView22.setTypeface(typeface2)
+            textView22.setTextSize(TypedValue.COMPLEX_UNIT_SP,16F);
+            textView22.setTextColor(Color.parseColor("#C4FFFFFF"))
+            textView22.text = tweet.timestamp
+            linearLayout2.addView(textView22)
+
+            //ikonka Twittera
+            val imageButton = ImageButton(requireContext())
+            val imgbtn_params = LayoutParams(
+                LayoutParams.WRAP_CONTENT, // CardView width
+                LayoutParams.WRAP_CONTENT
+            )
+//            textView12.setPadding(25,10,25,10)
+            imgbtn_params.setMargins(0,0,20,-10)
+            imgbtn_params.gravity = Gravity.END or Gravity.BOTTOM  // bottom-right
+            imageButton.layoutParams = imgbtn_params
+            imageButton.setImageResource(R.drawable.ic_twitter_small)
+            imageButton.setBackgroundColor(Color.parseColor("#00FFFFFF"))
+
+            imageButton.setOnClickListener(View.OnClickListener {
+                val browserIntent =
+                    Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/MZ_GOV_PL/status/" + tweet.tweetID))
+                startActivity(browserIntent) })
+            linearLayout2.addView(imageButton)
 
             //textview do pierwszego LL
             val textView = TextView(requireContext())
