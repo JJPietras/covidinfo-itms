a81facf250b893109aa52e3e48d6d11f3c30c7ed
Jarosław Drzymalski <jarek.drzymalski@gmail.com>
2020-12-10 02:09:34 +0100

    Zrobiony TT, fixnąć layout

--- COVIDInfo/app/src/main/java/com/drzymalski/covidinfo/ui/twitter/TwitterFragment.kt
+++ COVIDInfo/app/src/main/java/com/drzymalski/covidinfo/ui/twitter/TwitterFragment.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.StrictMode
 import android.util.TypedValue
 import android.view.Gravity
@@ -25,11 +27,15 @@ import com.drzymalski.covidinfo.lib.FragmentBinder
 import com.drzymalski.covidinfo.ui.selector.SelectorFragment
 import io.github.nandandesai.twitterscraper4j.TwitterScraper
 import kotlinx.android.synthetic.main.fragment_tweeter.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class TwitterFragment : Fragment() {
 
     private lateinit var twitterViewModel: TwitterViewModel
+    public var curTTacc = "MZ_GOV_PL"
+    public var curTTcount = 0
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -47,138 +53,183 @@ class TwitterFragment : Fragment() {
             SelectorFragment(),
             requireActivity()
         )
+        twitterRefreshBtn.setOnClickListener {
+            refresh()
+        }
+        twitterPreviousAccount.setOnClickListener {
+            switch(2)
+        }
+        twitterNextAccount.setOnClickListener {
+            switch(1)
+        }
+
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
-        getPosts()
-    }
-
-    private fun getPosts() {
-        val twitterScraper = TwitterScraper.builder().build()
-        val tweets = twitterScraper.getUserTimeline("MZ_GOV_PL")
-        for (tweet in tweets) {
-            println(tweet)
-            val cardView = CardView(requireContext())
-
-            // Initialize a new LayoutParams instance, CardView width and height
-            val cvLayoutParams = LayoutParams(
-                LayoutParams.MATCH_PARENT, // CardView width
-                LayoutParams.WRAP_CONTENT // CardView height
-            )
-
-            cvLayoutParams.setMargins(0,40,0,0)
-            cardView.layoutParams = cvLayoutParams
-            //cardView.setContentPadding(25,25,25,25)
-
-            cardView.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.colorPrimary))
-            cardView.radius = 170F
-            twitterLayout.addView(cardView)
-            //tło
-            val imageView = ImageView(requireContext())
-
-            imageView.setImageResource(R.drawable.side_nav_bar)
-
-            imageView.layoutParams = LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                LayoutParams.MATCH_PARENT
-            )
-
-            cardView.addView(imageView)
-            //pierwszy LL
-            val linearLayout = LinearLayout(requireContext())
-
-            linearLayout.layoutParams = LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                LayoutParams.MATCH_PARENT
-            )
-
-            linearLayout.orientation = LinearLayout.VERTICAL
-
-            cardView.addView(linearLayout)
-
-
-            //drugi LL
-            val linearLayout2 = LinearLayout(requireContext())
-
-            linearLayout2.layoutParams = LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                LayoutParams.WRAP_CONTENT
-            )
-            linearLayout2.orientation = LinearLayout.HORIZONTAL
-            linearLayout.addView(linearLayout2)
-
-            //1 textView w drugim LL
-            val textView12 = TextView(requireContext())
-            val tv12LayoutParams = LayoutParams(
-                LayoutParams.WRAP_CONTENT, // CardView width
-                LayoutParams.WRAP_CONTENT // CardView height
-            )
-            tv12LayoutParams.setMargins(80, 60, 0, 0)
-//            textView12.setPadding(25,10,25,10)
-            textView12.layoutParams = tv12LayoutParams
-            val typeface = ResourcesCompat.getFont(requireContext(), R.font.roboto_medium)
-            textView12.typeface = typeface
-            textView12.setTextSize(TypedValue.COMPLEX_UNIT_SP,20F)
-            textView12.setTextColor(Color.parseColor("#7EFFFFFF"))
-            textView12.text = "Ministerstwo Zdrowia"
-            linearLayout2.addView(textView12)
-
-            //2 textView w drugim LL
-            val textView22 = TextView(requireContext())
-            val tv22LayoutParams = LayoutParams(
-                0, // CardView width
-                LayoutParams.WRAP_CONTENT,
-                0.1F // CardView height
-            )
-            tv22LayoutParams.setMargins(40, 60, 0, 0)
-            tv22LayoutParams.weight = 0.1F
-            textView22.layoutParams = tv22LayoutParams
-
-            val typeface2 = ResourcesCompat.getFont(requireContext(), R.font.roboto)
-            textView22.typeface = typeface2
-            textView22.setTextSize(TypedValue.COMPLEX_UNIT_SP,16F)
-            textView22.setTextColor(Color.parseColor("#C4FFFFFF"))
-            textView22.text = tweet.timestamp
-            linearLayout2.addView(textView22)
-
-            //ikonka Twittera
-            val imageButton = ImageButton(requireContext())
-            val imgBtnParams = LayoutParams(
-                LayoutParams.WRAP_CONTENT, // CardView width
-                LayoutParams.WRAP_CONTENT
-            )
-            imgBtnParams.setMargins(0,30,100,0)
-            imgBtnParams.gravity = Gravity.END or Gravity.BOTTOM  // bottom-right
-            imageButton.layoutParams = imgBtnParams
-            imageButton.setImageResource(R.drawable.ic_twitter_small)
-            imageButton.setBackgroundColor(Color.parseColor("#00FFFFFF"))
-
-            imageButton.setOnClickListener {
-                val browserIntent =
-                    Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("https://twitter.com/MZ_GOV_PL/status/" + tweet.tweetID)
-                    )
-                startActivity(browserIntent)
-            }
-            linearLayout2.addView(imageButton)
-
-            //textview do pierwszego LL
-            val textView = TextView(requireContext())
-            val tvLayoutParams = LayoutParams(
-                LayoutParams.MATCH_PARENT, // CardView width
-                LayoutParams.WRAP_CONTENT // CardView height
-            )
-            tvLayoutParams.setMargins(80, 0, 80, 0)
-            textView.setPadding(0,0,0,100)
-            textView.layoutParams = tvLayoutParams
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,18F)
-            textView.setTextColor(Color.parseColor("#CBFFFFFF"))
-            textView.text = tweet.tweetText
-            linearLayout.addView(textView)
-
+        GlobalScope.launch {
+            loadData()
+            getPosts()
         }
     }
 
+    private fun loadData() {
+        Handler(Looper.getMainLooper()).post(Runnable {
+            val twitterScraper = TwitterScraper.builder().build()
+            val profile = twitterScraper.getProfile(curTTacc)
+            twitterAccountTitle.text = profile.name
+            twitterAccountDescription.text = profile.description
+            twitterAccountUsername.text = profile.username
+            twitterObservedCount.text = profile.noOfFollowers.toString()
+            twitterObservesCount.text = profile.noOfFriends.toString()
+        })
+    }
+
+    private fun getPosts() {
+        Handler(Looper.getMainLooper()).post(Runnable {
+            val twitterScraper = TwitterScraper.builder().build()
+            val tweets = twitterScraper.getUserTimeline(curTTacc)
+            for (tweet in tweets) {
+                val cardView = CardView(requireContext())
+                // Initialize a new LayoutParams instance, CardView width and height
+                val cvLayoutParams = LayoutParams(
+                    LayoutParams.MATCH_PARENT, // CardView width
+                    LayoutParams.WRAP_CONTENT // CardView height
+                )
+
+                cvLayoutParams.setMargins(0,40,0,0)
+                cardView.layoutParams = cvLayoutParams
+                //cardView.setContentPadding(25,25,25,25)
+
+                cardView.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.colorPrimary))
+                cardView.radius = 100F
+                twitterLayout.addView(cardView)
+                //tło
+                val imageView = ImageView(requireContext())
+
+                imageView.setImageResource(R.drawable.side_nav_bar)
+
+                imageView.layoutParams = LayoutParams(
+                    LayoutParams.MATCH_PARENT,
+                    LayoutParams.MATCH_PARENT
+                )
+
+                cardView.addView(imageView)
+                //pierwszy LL
+                val linearLayout = LinearLayout(requireContext())
+
+                linearLayout.layoutParams = LayoutParams(
+                    LayoutParams.MATCH_PARENT,
+                    LayoutParams.MATCH_PARENT
+                )
+
+                linearLayout.orientation = LinearLayout.VERTICAL
+
+                cardView.addView(linearLayout)
+
+
+                //drugi LL
+                val linearLayout2 = LinearLayout(requireContext())
+
+                linearLayout2.layoutParams = LayoutParams(
+                    LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT
+                )
+                linearLayout2.orientation = LinearLayout.HORIZONTAL
+                linearLayout.addView(linearLayout2)
+
+                //1 textView w drugim LL
+                val textView12 = TextView(requireContext())
+                val tv12LayoutParams = LayoutParams(
+                    LayoutParams.WRAP_CONTENT, // CardView width
+                    LayoutParams.WRAP_CONTENT // CardView height
+                )
+                tv12LayoutParams.setMargins(60, 60, 0, 0)
+//            textView12.setPadding(25,10,25,10)
+                textView12.layoutParams = tv12LayoutParams
+                val typeface = ResourcesCompat.getFont(requireContext(), R.font.roboto_medium)
+                textView12.typeface = typeface
+                textView12.setTextSize(TypedValue.COMPLEX_UNIT_SP,20F)
+                textView12.setTextColor(Color.parseColor("#7EFFFFFF"))
+                textView12.text = "@" + curTTacc
+                linearLayout2.addView(textView12)
+
+                //2 textView w drugim LL
+                val textView22 = TextView(requireContext())
+                val tv22LayoutParams = LayoutParams(
+                    0, // CardView width
+                    LayoutParams.WRAP_CONTENT,
+                    0.1F // CardView height
+                )
+                tv22LayoutParams.setMargins(40, 60, 0, 0)
+                tv22LayoutParams.weight = 0.1F
+                textView22.layoutParams = tv22LayoutParams
+
+                val typeface2 = ResourcesCompat.getFont(requireContext(), R.font.roboto)
+                textView22.typeface = typeface2
+                textView22.setTextSize(TypedValue.COMPLEX_UNIT_SP,16F)
+                textView22.setTextColor(Color.parseColor("#C4FFFFFF"))
+                textView22.text = tweet.timestamp
+                linearLayout2.addView(textView22)
+
+                //ikonka Twittera
+                val imageButton = ImageButton(requireContext())
+                val imgBtnParams = LayoutParams(
+                    LayoutParams.WRAP_CONTENT, // CardView width
+                    LayoutParams.WRAP_CONTENT
+                )
+                imgBtnParams.setMargins(0,30,100,0)
+                imgBtnParams.gravity = Gravity.END or Gravity.BOTTOM  // bottom-right
+                imageButton.layoutParams = imgBtnParams
+                imageButton.setImageResource(R.drawable.ic_twitter_small)
+                imageButton.setBackgroundColor(Color.parseColor("#00FFFFFF"))
+
+                imageButton.setOnClickListener {
+                    val browserIntent =
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("https://twitter.com/MZ_GOV_PL/status/" + tweet.tweetID)
+                        )
+                    startActivity(browserIntent)
+                }
+                linearLayout2.addView(imageButton)
+
+                //textview do pierwszego LL
+                val textView = TextView(requireContext())
+                val tvLayoutParams = LayoutParams(
+                    LayoutParams.MATCH_PARENT, // CardView width
+                    LayoutParams.WRAP_CONTENT // CardView height
+                )
+                tvLayoutParams.setMargins(50, 0, 80, 0)
+                textView.setPadding(0,0,0,50)
+                textView.layoutParams = tvLayoutParams
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,18F)
+                textView.setTextColor(Color.parseColor("#CBFFFFFF"))
+                textView.text = tweet.tweetText
+                linearLayout.addView(textView)
+
+            }
+
+        })
+    }
+
+    private fun refresh(){
+        twitterLayout.removeViews(1,twitterLayout.getChildCount()-1)
+        loadData()
+        getPosts()
+    }
+
+    private fun switch(value: Int) {
+        curTTcount=curTTcount+value
+        if (curTTcount % 3 == 0 ){
+            curTTacc = "MZ_GOV_PL"
+        }
+        else if (curTTcount % 3 == 1 ){
+            curTTacc = "GIS_gov"
+        }
+        else if (curTTcount % 3 == 2 ){
+            curTTacc = "WHO"
+        }
+        refresh()
+    }
 
 
 
