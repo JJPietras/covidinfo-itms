1c8b281ce00506ac9220acd12714c3d94cb3e260
jakub_pietras <jacubeus@gmail.com>
2020-12-17 05:29:40 +0100

    Finished styling and hospitals fragment.

--- COVIDInfo/app/src/main/java/com/drzymalski/covidinfo/ui/hospitals/HospitalsFragment.kt
+++ COVIDInfo/app/src/main/java/com/drzymalski/covidinfo/ui/hospitals/HospitalsFragment.kt
@@ -1,16 +1,17 @@
 package com.drzymalski.covidinfo.ui.hospitals
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
@@ -23,7 +24,6 @@ import com.drzymalski.covidinfo.ui.selector.SelectorFragment
 import kotlinx.android.synthetic.main.fragment_hospitals.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.math.abs
@@ -35,6 +35,7 @@ class HospitalsFragment : Fragment() {
     private lateinit var hospitalsViewModel: HospitalsViewModel
 
     private lateinit var images: Array<Drawable?>
+    private lateinit var imageIndexes: Array<Int>
     private var currentName: String = "NONE"
     private val regions = Hospitals.regionHospitals.keys
     private var currentList: List<Hospital>? = null
@@ -63,6 +64,9 @@ class HospitalsFragment : Fragment() {
             ResourcesCompat.getDrawable(resources, R.drawable.ic_small_hospital, null),
             ResourcesCompat.getDrawable(resources, R.drawable.ic_hospitals, null)
         )
+
+        imageIndexes = arrayOf(R.drawable.ic_small_hospital, R.drawable.ic_hospitals)
+
 
         var count = 0
         for (region in Hospitals.regionHospitals)
@@ -182,53 +186,58 @@ class HospitalsFragment : Fragment() {
     }
 
     private fun createHospitalEntry(hospital: Hospital) {
-        val wrapperLayout = LinearLayout(requireContext())
-        val params = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
+        val wrapperLayout = ConstraintLayout(requireContext())
+        wrapperLayout.id = View.generateViewId()
+        val params = ConstraintLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
         )
-        wrapperLayout.gravity = Gravity.CENTER
+        params.bottomMargin = 60
         wrapperLayout.layoutParams = params
-        wrapperLayout.orientation = LinearLayout.HORIZONTAL
-        wrapperLayout.weightSum = 1F
+        hospitalsLayout.addView(wrapperLayout)
+        hospitalsLayout.visibility = View.VISIBLE
 
+        //==============//
         //Hospital button
+        //==============//
 
-        val hospitalBtn = Button(requireContext())
+        val hospitalBtn = ImageButton(requireContext())
+        wrapperLayout.addView(hospitalBtn)
+        hospitalBtn.id = View.generateViewId()
+
         hospitalBtn.setOnClickListener { switchHospital(hospital) }
-        hospitalBtn.background = images[abs(Random.nextInt() % 2)]
-        hospitalBtn.scaleX = 0.5F
-        hospitalBtn.scaleY = 0.5F
+        hospitalBtn.setImageResource(imageIndexes[abs(Random.nextInt() % 2)])
 
-        val hospitalBtnLayout = LinearLayout.LayoutParams(
-            0,
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            0.22F
-        )
-        hospitalBtn.layoutParams = hospitalBtnLayout
-        /*hospitalBtn.width = 256*/
-        hospitalBtn.minWidth = 58
+        val hospitalBtnLayout = ConstraintLayout.LayoutParams(260, 260)
+        hospitalBtn.layoutParams = hospitalBtnLayout //
+        hospitalBtn.scaleType = ImageView.ScaleType.FIT_CENTER
+        hospitalBtn.background = null
 
+
+        //==============//
         //Hospital layout
+        //==============//
 
         val typeface = ResourcesCompat.getFont(requireContext(), R.font.roboto_medium)
         val textLayout = LinearLayout(requireContext())
-        val textLayoutParams = LinearLayout.LayoutParams(
-            0,
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            0.36F
-        )
 
-        textLayout.layoutParams = textLayoutParams
+        wrapperLayout.addView(textLayout)
+        textLayout.id = View.generateViewId()
+
+        val textLayoutParams = ConstraintLayout.LayoutParams(
+            0,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
         textLayout.orientation = LinearLayout.VERTICAL
+        textLayout.layoutParams = textLayoutParams//
 
         //First text
 
         val hospitalName = TextView(requireContext())
+        textLayout.addView(hospitalName)
         val hospitalNameParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            0.25F
+            LinearLayout.LayoutParams.WRAP_CONTENT
         )
         hospitalNameParams.topMargin = 30
         hospitalName.layoutParams = hospitalNameParams
@@ -241,10 +250,10 @@ class HospitalsFragment : Fragment() {
         //Second text
 
         val hospitalTitle = TextView(requireContext())
+        textLayout.addView(hospitalTitle)
         val hospitalTitleParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            0.15F
+            LinearLayout.LayoutParams.WRAP_CONTENT
         )
         hospitalTitle.layoutParams = hospitalTitleParams
         hospitalTitle.typeface = typeface
@@ -252,13 +261,28 @@ class HospitalsFragment : Fragment() {
         hospitalTitle.setTextSize(android.util.TypedValue.COMPLEX_UNIT_SP, 13F)
         hospitalTitle.text = hospital.title
 
+
         //Third text
 
+        val hospitalRegionCity = TextView(requireContext())
+        textLayout.addView(hospitalRegionCity)
+        val hospitalRegionCityParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        hospitalRegionCity.layoutParams = hospitalRegionCityParams
+        hospitalRegionCity.typeface = typeface
+        hospitalRegionCity.setTextColor(Color.parseColor("#707070"))
+        hospitalRegionCity.setTextSize(android.util.TypedValue.COMPLEX_UNIT_SP, 13F)
+        hospitalRegionCity.text = hospital.city
+
+        //Forth text
+
         val hospitalContact = TextView(requireContext())
+        textLayout.addView(hospitalContact)
         val hospitalContactParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            0.25F
+            LinearLayout.LayoutParams.WRAP_CONTENT
         )
         hospitalContact.layoutParams = hospitalContactParams
         hospitalContact.typeface = typeface
@@ -266,35 +290,86 @@ class HospitalsFragment : Fragment() {
         hospitalContact.setTextSize(android.util.TypedValue.COMPLEX_UNIT_SP, 13F)
         hospitalContact.text = hospital.contact
 
-        //TODO: add city and maybe region
-
+        //==============//
         //Google button
+        //==============//
 
         val googleBtn = Button(requireContext())
-        googleBtn.setOnClickListener { switchHospital(hospital) }
-        googleBtn.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_map_point, null)
-        googleBtn.scaleX = 0.7F
-        googleBtn.scaleY = 0.7F
+        wrapperLayout.addView(googleBtn)
+        googleBtn.id = View.generateViewId()
 
-        val googleBtnLayout = LinearLayout.LayoutParams(
-            0,
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            0.1F
-        )
-        hospitalBtn.layoutParams = googleBtnLayout
+        googleBtn.setOnClickListener {
+            val contact = hospital.contact
+            val cords = hospital.location
+            val route = contact.substring(0, contact.indexOf(','))
+            val output = StringBuilder("geo:${cords[0]},${cords[1]}?q=")
+            val s = ",+"
+
+            output.append(hospital.city).append(s).append(hospital.name).append(s)
+            output.append(hospital.title).append(s).append(route).append(s)
+            val location = Uri.parse(output.toString())
+            val mapIntent = Intent(Intent.ACTION_VIEW, location)
+            val intentTitle = "Open with:"
+            val chooser = Intent.createChooser(mapIntent, intentTitle)
+            startActivity(chooser)
+        }
+        googleBtn.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_map_point, null)
+
+        val googleBtnLayout = ConstraintLayout.LayoutParams(168, 180)
+        googleBtn.layoutParams = googleBtnLayout
 
         //Adding
 
-        textLayout.addView(hospitalName)
-        textLayout.addView(hospitalTitle)
-        textLayout.addView(hospitalContact)
-        wrapperLayout.addView(hospitalBtn)
-        wrapperLayout.addView(textLayout)
-        wrapperLayout.addView(googleBtn)
-        hospitalsLayout.addView(wrapperLayout)
+        val layoutSet = ConstraintSet()
+        layoutSet.clone(wrapperLayout)
 
-        textLayout.requestLayout()
+        layoutSet.connect(
+            hospitalBtn.id,
+            ConstraintSet.BOTTOM,
+            wrapperLayout.id,
+            ConstraintSet.BOTTOM
+        )
+        layoutSet.connect(
+            hospitalBtn.id,
+            ConstraintSet.START,
+            wrapperLayout.id,
+            ConstraintSet.START
+        )
+        layoutSet.connect(hospitalBtn.id, ConstraintSet.TOP, wrapperLayout.id, ConstraintSet.TOP)
+        layoutSet.connect(hospitalBtn.id, ConstraintSet.END, textLayout.id, ConstraintSet.START)
+
+        layoutSet.connect(
+            textLayout.id,
+            ConstraintSet.BOTTOM,
+            wrapperLayout.id,
+            ConstraintSet.BOTTOM
+        )
+        layoutSet.connect(textLayout.id, ConstraintSet.END, googleBtn.id, ConstraintSet.START)
+        layoutSet.connect(textLayout.id, ConstraintSet.START, hospitalBtn.id, ConstraintSet.END)
+        layoutSet.connect(textLayout.id, ConstraintSet.TOP, wrapperLayout.id, ConstraintSet.TOP)
+
+        layoutSet.connect(
+            googleBtn.id,
+            ConstraintSet.BOTTOM,
+            wrapperLayout.id,
+            ConstraintSet.BOTTOM
+        )
+        layoutSet.connect(googleBtn.id, ConstraintSet.END, wrapperLayout.id, ConstraintSet.END)
+        layoutSet.connect(googleBtn.id, ConstraintSet.TOP, wrapperLayout.id, ConstraintSet.TOP)
+
+   /*     layoutSet.setMargin(hospitalBtn.id, ConstraintSet.START, 40)
+        layoutSet.setMargin(hospitalBtn.id, ConstraintSet.END, 40)*/
+
+        layoutSet.setMargin(textLayout.id, ConstraintSet.START, -120)
+        layoutSet.setMargin(textLayout.id, ConstraintSet.END, -120)
+
+        layoutSet.setMargin(googleBtn.id, ConstraintSet.START, 40)
+        layoutSet.setMargin(googleBtn.id, ConstraintSet.END, 40)
+
+        layoutSet.applyTo(wrapperLayout)
+
+        /*textLayout.requestLayout()
         wrapperLayout.requestLayout()
-        hospitalsLayout.requestLayout()
+        hospitalsLayout.requestLayout()*/
     }
 }
